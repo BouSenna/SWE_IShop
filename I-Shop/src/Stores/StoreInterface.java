40cd52d6e831267377501c8a61966dbe31fe7ddb
BouSenna <sara_usa_1999@hotmail.com>
2019-11-09 13:21:37 +0200

    More fixex in store interface

--- I-Shop/src/Stores/StoreInterface.java
+++ I-Shop/src/Stores/StoreInterface.java
@@ -13,5 +13,8 @@ public interface StoreInterface {
     public void setAddress(String address);
     public String getAddress();
     
+    public String getmApproval();
+	public void setmApproval(String mApproval);
+    
     public void addStore(String Name, String Category, String Type, String address, int license);
 }
