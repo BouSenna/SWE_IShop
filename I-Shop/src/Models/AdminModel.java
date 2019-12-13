b275f29427f4b6c3837caec41c1d507d24e804f5
BouSenna <sara_usa_1999@hotmail.com>
2019-12-14 01:52:02 +0200

    Added Class AdminModel.

---
+++ I-Shop/src/Models/AdminModel.java
@@ -0,0 +1,7 @@
+package Models;
+
+///This class is responsible for retrieving and updating the data from the Administrator table
+///in the database and converting it to a meaningful concept for our software.
+public class AdminModel {
+	public AdminModel() {}
+}
