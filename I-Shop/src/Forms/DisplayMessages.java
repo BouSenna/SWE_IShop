bb1d09d256de15be0403f98e7d3c4a8677462e53
BouSenna <sara_usa_1999@hotmail.com>
2019-12-13 16:25:44 +0200

    Added class display messages in the view package.

---
+++ I-Shop/src/Forms/DisplayMessages.java
@@ -0,0 +1,11 @@
+package Forms;
+
+import javax.swing.JOptionPane;
+
+public class DisplayMessages {
+	public DisplayMessages() {}
+	public void displayMessage(String message) {
+		JOptionPane.showMessageDialog(null, message);
+
+	}
+}
