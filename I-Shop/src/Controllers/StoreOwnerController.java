0784252342bba5a1ea126cf7754ad59e0ff3047d
BouSenna <sara_usa_1999@hotmail.com>
2019-12-14 00:27:15 +0200

    Added class StoreOwnerController.

---
+++ I-Shop/src/Controllers/StoreOwnerController.java
@@ -0,0 +1,8 @@
+package Controllers;
+
+///This class handles requests from users. it is responsible for rendering a response
+///with the aid of the StoreOwnerView and StoreOwnerModel.
+public class StoreOwnerController {
+	public StoreOwnerController(){}
+	
+}
