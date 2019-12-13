f3c1ec89eb7442bb94f6387b1aa016dc0fbe457b
BouSenna <sara_usa_1999@hotmail.com>
2019-12-13 20:46:10 +0200

    Added getItemID that retrieves the ID of the item from the DB.

--- I-Shop/src/Models/ItemModel.java
+++ I-Shop/src/Models/ItemModel.java
@@ -1,8 +1,39 @@
 package Models;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Database.DBConnect;
+
 ///This class is responsible for retrieving the data from the items table
 ///in the database and converting it to a meaningful concept for our software.
 public class ItemModel {
+	Connection connection = null;
+	Statement stmt = null;
+	ResultSet resultset = null;
+
 	public void ItemModel() {
+	}
+
+	/// Method to return the item ID.
+	public int getItemID(String itemName) {
+		/// Establishing a connection with the database.
+		connection = DBConnect.DBConnect();
+		int ID = -1;
+		try {
+			stmt = connection.createStatement();
+			/// Getting the ID of an item with the given item name
+			/// and storing it in variable ID
+			resultset = stmt.executeQuery("select ItemID from Item where ItemName ='" + itemName + "'");
+			while (resultset.next()) {
+				ID = resultset.getInt(1);
+			}
+
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		return ID;
 	}
 }
