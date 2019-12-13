bd83eb3e1d63ec27f110a2d555d8dd31f2562230
BouSenna <sara_usa_1999@hotmail.com>
2019-12-13 16:31:53 +0200

    Added class ProductModel.

---
+++ I-Shop/src/Models/ProductModel.java
@@ -0,0 +1,9 @@
+package Models;
+
+
+///This class is responsible for retrieving the data from the product table in the database and converting
+///it to a meaningful concept for our software.
+public class ProductModel {
+	public ProductModel() {
+	}
+}
