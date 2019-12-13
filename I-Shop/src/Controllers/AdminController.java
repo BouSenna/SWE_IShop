7fb38b7ee9df246eb2d218be3b8a847752a2d0ec
BouSenna <sara_usa_1999@hotmail.com>
2019-12-14 01:48:19 +0200

    Added class AdminController.

---
+++ I-Shop/src/Controllers/AdminController.java
@@ -0,0 +1,7 @@
+package Controllers;
+
+///This class handles requests from users. it is responsible for rendering a response
+///with the aid of the AdminView and AdminModel.
+public class AdminController {
+	public AdminController(){}
+}
