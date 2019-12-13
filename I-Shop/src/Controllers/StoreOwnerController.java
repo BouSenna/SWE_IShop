53adb3424d0c36ef8f5a98e52d71dcfb393f70dd
BouSenna <sara_usa_1999@hotmail.com>
2019-12-14 01:33:51 +0200

    added register method to the StoreOwnerController.

--- I-Shop/src/Controllers/StoreOwnerController.java
+++ I-Shop/src/Controllers/StoreOwnerController.java
@@ -1,8 +1,20 @@
 package Controllers;
 
+import java.sql.SQLException;
+
+import Database.DBConnect;
+import Models.StoreOwnerModel;
+
 ///This class handles requests from users. it is responsible for rendering a response
 ///with the aid of the StoreOwnerView and StoreOwnerModel.
 public class StoreOwnerController {
-	public StoreOwnerController(){}
-	
+	public StoreOwnerController() {
+	}
+
+	/// Add store owner account.
+	public void register(int UserID, String UserEmail, String UserPassword, String Name, int NumOfStores,
+			int License) {
+		new StoreOwnerModel().addStoreOwner(UserID, UserEmail, UserPassword, Name, NumOfStores, License);
+	}
+
 }
