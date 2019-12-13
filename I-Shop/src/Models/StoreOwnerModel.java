edffd4a41b58263d39809c45cf2b47c84afb6288
BouSenna <sara_usa_1999@hotmail.com>
2019-12-14 00:38:21 +0200

    Added StoreOwnerModel.

---
+++ I-Shop/src/Models/StoreOwnerModel.java
@@ -0,0 +1,7 @@
+package Models;
+
+///This class is responsible for retrieving the data from the store owner table in the database and converting
+///it to a meaningful concept for our software.
+public class StoreOwnerModel {
+	public StoreOwnerModel() {}
+}
