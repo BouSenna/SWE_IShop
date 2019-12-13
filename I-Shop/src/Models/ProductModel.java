388a16d6d8f91ded494052096efb0538ec9b604e
BouSenna <sara_usa_1999@hotmail.com>
2019-12-13 16:34:37 +0200

    Added getQuantity method that retrieve the quantity of product from DB.

--- I-Shop/src/Models/ProductModel.java
+++ I-Shop/src/Models/ProductModel.java
@@ -1,9 +1,35 @@
 package Models;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Database.DBConnect;
 
 ///This class is responsible for retrieving the data from the product table in the database and converting
 ///it to a meaningful concept for our software.
 public class ProductModel {
 	public ProductModel() {
+	}
+
+	/// Method to return the available quantity of a product.
+	public int getQuantity(String myID) {
+		/// Establishing a connection with the database.
+		Connection connection = DBConnect.DBConnect();
+		int quantity = -1;
+		try {
+			Statement stmt = connection.createStatement();
+			/// Getting the available quantity of the product with the given product ID
+			/// and storing it in variable quantity
+			ResultSet resultset = stmt.executeQuery("Select Quantity from Product where ProductID ='" + myID + "'");
+			while (resultset.next()) {
+				quantity = resultset.getInt(1);
+			}
+
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		return quantity;
 	}
 }
