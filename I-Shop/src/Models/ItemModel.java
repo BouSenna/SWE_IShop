91342340f8f1a4572773c5fcee4b25f26a361436
BouSenna <sara_usa_1999@hotmail.com>
2019-12-13 20:40:48 +0200

    Added class ItemModel.

---
+++ I-Shop/src/Models/ItemModel.java
@@ -0,0 +1,8 @@
+package Models;
+
+///This class is responsible for retrieving the data from the items table
+///in the database and converting it to a meaningful concept for our software.
+public class ItemModel {
+	public void ItemModel() {
+	}
+}
