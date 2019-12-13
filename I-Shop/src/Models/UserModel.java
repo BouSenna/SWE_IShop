03311ec5b2278952d8b866d1e3177f44552b1d9a
BouSenna <sara_usa_1999@hotmail.com>
2019-12-13 23:40:22 +0200

    Added method checkLoginInfo that searches which user is trying to login.

--- I-Shop/src/Models/UserModel.java
+++ I-Shop/src/Models/UserModel.java
@@ -1,8 +1,40 @@
 package Models;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Database.DBConnect;
+import Enums.UserTypes;
+
 public class UserModel {
+	Connection connection = null;
+	Statement stmt = null;
+	ResultSet resultset = null;
 
 	public UserModel() {
 	}
 
+	public int checkLoginInfo(String Email, String password) {
+		try {
+			/// Establishing a connection with the database.
+			connection = DBConnect.DBConnect();
+			stmt = connection.createStatement();
+
+			/// Searching which user type is trying to login and returning the ordinal.
+			for (UserTypes type : UserTypes.values()) {
+				resultset = stmt.executeQuery("select email,password from [User] inner " + "join " + type.myString()
+						+ " on [user].UserID=" + type.myString() + ".UserID");
+				while (resultset.next()) {
+					if (resultset.getString("email").equals(Email) && resultset.getString("password").equals(password))
+						return type.ordinal();
+				}
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return -1;
+	}
 }
