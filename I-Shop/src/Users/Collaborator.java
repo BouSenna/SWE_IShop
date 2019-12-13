8958de534a21b39a7173846be1f9ee85a364cee9
BouSenna <sara_usa_1999@hotmail.com>
2019-12-13 22:54:27 +0200

    Added accessors and mutators.

--- I-Shop/src/Users/Collaborator.java
+++ I-Shop/src/Users/Collaborator.java
@@ -1,5 +1,32 @@
 package Users;
 
 public class Collaborator extends User implements CollaboratorInterface {
-	
+	String mName;
+	int mLicense;
+
+	/// Constructor
+	public Collaborator() {
+	}
+
+	public Collaborator(String Name, int License) {
+		this.mLicense = License;
+		this.mName = Name;
+	}
+
+	/// Mutators and accessors.
+	public String getmName() {
+		return mName;
+	}
+
+	public void setmName(String mName) {
+		this.mName = mName;
+	}
+
+	public int getmLicense() {
+		return mLicense;
+	}
+
+	public void setmLicense(int mLicense) {
+		this.mLicense = mLicense;
+	}
 }
--- I-Shop/src/Users/CollaboratorInterface.java
+++ I-Shop/src/Users/CollaboratorInterface.java
@@ -1,5 +1,10 @@
 package Users;
 
 public interface CollaboratorInterface {
+	/// Mutators and accessors.
+	public String getmName();
+	public void setmName(String mName);
 
+	public int getmLicense();
+	public void setmLicense(int mLicense);
 }
