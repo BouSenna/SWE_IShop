894ebd3b58ae7578578b7ae655f69c40c9c738ac
BouSenna <sara_usa_1999@hotmail.com>
2019-12-15 01:36:39 +0200

    Added class BrandController.

---
+++ I-Shop/src/Controllers/BrandController.java
@@ -0,0 +1,9 @@
+package Controllers;
+
+///This class handles requests from users, it is responsible for rendering a response
+///with the aid of the BrandView and BrandModel.
+public class BrandController {
+	/// Constructor
+	public BrandController() {}
+
+}
