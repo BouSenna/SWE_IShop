be547f53241e64c37fce5003353284ee80ea0546
BouSenna <sara_usa_1999@hotmail.com>
2019-12-14 02:10:35 +0200

    Added Class StoreModel.

---
+++ I-Shop/src/Models/StoreModel.java
@@ -0,0 +1,8 @@
+package Models;
+
+///This class is responsible for retrieving and updating the data from the Store table
+///in the database and converting it to a meaningful concept for our software.
+public class StoreModel {
+	public StoreModel() {}
+	
+}
