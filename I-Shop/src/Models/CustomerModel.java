a965d50209adf5a8060d7216245e092f8917f261
BouSenna <sara_usa_1999@hotmail.com>
2019-12-14 19:07:06 +0200

    Added addNewCustomer method that adds customers to the database.

--- I-Shop/src/Models/CustomerModel.java
+++ I-Shop/src/Models/CustomerModel.java
@@ -11,6 +11,10 @@ import Database.DBConnect;
 ///This class is responsible for retrieving the data from the customer table in the database and converting
 ///it to a meaningful concept for our software.
 public class CustomerModel {
+	Connection connection = null;
+	Statement stmt = null;
+	ResultSet resultset = null;
+
 	public CustomerModel() {
 	}
 
@@ -18,10 +22,10 @@ public class CustomerModel {
 	/// info.
 	public int getBalance(String CCInfo) {
 		/// Establishing a connection with the database.
-		Connection connection = DBConnect.DBConnect();
+		connection = DBConnect.DBConnect();
 		int balance = -1;
 		try {
-			Statement stmt = connection.createStatement();
+			stmt = connection.createStatement();
 			/// Getting the account balance of the customer with the given credit card info
 			/// and storing it in balance arrayList.
 			ResultSet resultset_customer = stmt
@@ -39,15 +43,38 @@ public class CustomerModel {
 	/// Method update the account balance of a customer in the database.
 	public void updateBalance(String CCInfo, int Balance) {
 		/// Establishing a connection with the database.
-		Connection connection = DBConnect.DBConnect();
+		connection = DBConnect.DBConnect();
 		try {
 			/// Updating the account balance of a customer -with the given credit card info-
 			/// with the new balance.
-			Statement stmt = connection.createStatement();
+			stmt = connection.createStatement();
 			stmt.executeUpdate(
 					"update Customer set AccountBalance='" + Balance + "' where CreditCardInfo ='" + CCInfo + "'");
 		} catch (SQLException e1) {
 			e1.printStackTrace();
+		}
+	}
+
+	/// Method to add new customer
+	public void addNewCustomer(String UserEmail, String UserPassword, String Name, String Address,
+			String CreditCardInfo, int MobileNumber, float AccountBalance) {
+		try {
+			int temp = 0;
+			stmt = connection.createStatement();
+			stmt.execute("insert into [User] (Email,Password) values ('" + UserEmail + "','" + UserPassword + "')");
+			resultset = stmt.executeQuery("select userid from [user] where email = '" + UserEmail + "' AND password = '"
+					+ UserPassword + "' ");
+			while (resultset.next())
+				temp = resultset.getInt(1);
+
+			stmt.execute(
+					"insert into customer  (UserID,CustomerName, Address,MobileNumber,AccountBalance,CreditCardinfo) values ('"
+							+ temp + "','" + Name + "','" + Address + "','" + MobileNumber + "','" + AccountBalance
+							+ "','" + CreditCardInfo + "')");
+			stmt.close();
+		} catch (SQLException e) {
+			System.out.println("Error executing query");
+			e.printStackTrace();
 		}
 	}
 }
