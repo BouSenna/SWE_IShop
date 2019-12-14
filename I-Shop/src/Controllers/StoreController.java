cb2d5ea429b468f5372b412ac6eb66ce7409ca8f
BouSenna <sara_usa_1999@hotmail.com>
2019-12-15 01:29:45 +0200

    Added class StoreController.

---
+++ I-Shop/src/Controllers/StoreController.java
@@ -0,0 +1,9 @@
+package Controllers;
+
+///This class handles requests from users, it is responsible for rendering a response
+///with the aid of the StoreView and StoreModel.
+public class StoreController {
+	///Constructor
+	public void StoreController(){}
+	
+}
