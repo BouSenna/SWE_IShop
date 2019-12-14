087ec89405cefbb14aced3bad07b3ccdf4e088e4
BouSenna <sara_usa_1999@hotmail.com>
2019-12-15 01:23:03 +0200

    Added add store method

--- I-Shop/src/Models/StoreModel.java
+++ I-Shop/src/Models/StoreModel.java
@@ -13,18 +13,35 @@ public class StoreModel {
 	Connection connection = null;
 	Statement stmt = null;
 	ResultSet resultset = null;
-	
-	public StoreModel() {}
-	
-	/// Method that updates the approval of a store in the database. 
+
+	public StoreModel() {
+	}
+
+	/// Method that updates the approval of a store in the database.
 	public void updateApproval(String newValue, String myID) {
 		try {
-    		connection = DBConnect.DBConnect();
-    		stmt = connection.createStatement();
-			stmt.executeUpdate("update Store set Approval='"+newValue+"' where StoreID='"+myID+"'");
+			connection = DBConnect.DBConnect();
+			stmt = connection.createStatement();
+			stmt.executeUpdate("update Store set Approval='" + newValue + "' where StoreID='" + myID + "'");
 		} catch (SQLException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+	}
+
+	/// Method that adds new store to the store table in the database
+	public void addStore(String StoreName, String Category, String StoreType, String Address, int license) {
+		connection = DBConnect.DBConnect();
+		String Approval = "no";
+
+		try {
+			stmt = connection.createStatement();
+			stmt.execute("insert into Store ( StoreName, StoreType, Address, Category,Approval) values " + "('"
+					+ StoreName + "','" + StoreType + "','" + Address + "','" + Category + "','" + Approval + "')");
+		} catch (SQLException e) {
+			System.out.println("Error executing query");
+			e.printStackTrace();
+		}
+
 	}
 }
