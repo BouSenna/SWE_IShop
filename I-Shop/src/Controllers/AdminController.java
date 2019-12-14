863cd9eb546a2b95280ff929dc8a599e0cbed7df
BouSenna <sara_usa_1999@hotmail.com>
2019-12-14 02:17:04 +0200

    Added confirmClassAddition method in the AdminController.

--- I-Shop/src/Controllers/AdminController.java
+++ I-Shop/src/Controllers/AdminController.java
@@ -1,7 +1,13 @@
 package Controllers;
 
+import Models.StoreModel;
+
 ///This class handles requests from users. it is responsible for rendering a response
 ///with the aid of the AdminView and AdminModel.
 public class AdminController {
 	public AdminController(){}
+	
+	public void confirmStoreAddition(String newValue, String myID) {
+    	new StoreModel().updateApproval(newValue, myID);
+    }
 }
