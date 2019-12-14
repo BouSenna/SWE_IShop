15fc7bb82204b43afc70aea151dd07157a917014
BouSenna <sara_usa_1999@hotmail.com>
2019-12-15 01:45:14 +0200

    Added method addBrand.

--- I-Shop/src/Controllers/BrandController.java
+++ I-Shop/src/Controllers/BrandController.java
@@ -1,9 +1,16 @@
 package Controllers;
 
+import Models.BrandModel;
+
 ///This class handles requests from users, it is responsible for rendering a response
 ///with the aid of the BrandView and BrandModel.
 public class BrandController {
 	/// Constructor
 	public BrandController() {}
+	
+	/// Method that adds new brand.
+	public void addBrand(String Name, String Category) {
+		new BrandModel().addBrand(Name, Category);;
+	}
 
 }
