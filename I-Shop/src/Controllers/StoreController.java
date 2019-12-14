b53e91b68f4db90a9080579f58735eabbe93d6cd
BouSenna <sara_usa_1999@hotmail.com>
2019-12-15 01:33:24 +0200

    Added addStore method to the controller.

--- I-Shop/src/Controllers/StoreController.java
+++ I-Shop/src/Controllers/StoreController.java
@@ -1,9 +1,14 @@
 package Controllers;
 
+import Models.StoreModel;
+
 ///This class handles requests from users, it is responsible for rendering a response
 ///with the aid of the StoreView and StoreModel.
 public class StoreController {
 	///Constructor
 	public void StoreController(){}
 	
+	public void addStore(String StoreName, String Category, String StoreType, String Address, int license) {
+		new StoreModel().addStore(StoreName, Category, StoreType, Address, license);
+	}
 }
