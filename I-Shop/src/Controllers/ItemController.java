af7c44e7ba0eac74c6a135340126ed4cdf711c14
BouSenna <sara_usa_1999@hotmail.com>
2019-12-15 02:30:31 +0200

    Added class ItemController.

---
+++ I-Shop/src/Controllers/ItemController.java
@@ -0,0 +1,7 @@
+package Controllers;
+
+///This class handles requests from users, it is responsible for rendering a response
+///with the aid of the ItemView and ItemController.
+public class ItemController {
+	public ItemController() {}
+}
