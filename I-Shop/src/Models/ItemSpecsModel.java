d131c7752d6b91cfe31f3ac9e435a3ddc3350996
BouSenna <sara_usa_1999@hotmail.com>
2019-12-15 05:16:09 +0200

    Added class ItemSpecsModel.

---
+++ I-Shop/src/Models/ItemSpecsModel.java
@@ -0,0 +1,8 @@
+package Models;
+
+///This class is responsible for retrieving the data from itemSpecs table
+///in the database and converting it to a meaningful concept for our software.
+public class ItemSpecsModel {
+	public ItemSpecsModel() {
+	}
+}
