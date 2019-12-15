b9deb52cf1bec3cf4db91cc8c89f03be95f3ee32
BouSenna <sara_usa_1999@hotmail.com>
2019-12-15 05:27:52 +0200

    added function that adds the item specifications to the database

--- I-Shop/src/Models/ItemSpecsModel.java
+++ I-Shop/src/Models/ItemSpecsModel.java
@@ -1,8 +1,31 @@
 package Models;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Database.DBConnect;
+
 ///This class is responsible for retrieving the data from itemSpecs table
 ///in the database and converting it to a meaningful concept for our software.
 public class ItemSpecsModel {
+	Connection connection = null;
+	Statement stmt = null;
+	ResultSet resultset = null;
+
 	public ItemSpecsModel() {
+	}
+
+	/// Method to add item specifications to its table in the database.
+	public void addItemSpects(String Name, String Category) {
+		/// Establishing a connection with the database.
+		connection = DBConnect.DBConnect();
+		try {
+			stmt = connection.createStatement();
+			stmt.execute("insert into itemspecs values ('" + Name + "','" + Category + "')");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
