ffc79b8a5b6dbc5d7f0a6897e8905609a8c084be
BouSenna <sara_usa_1999@hotmail.com>
2019-12-15 05:16:45 +0200

    Added class ItemSpecsController.

---
+++ I-Shop/src/Controllers/ItemSpecsController.java
@@ -0,0 +1,10 @@
+package Controllers;
+
+//This class handles requests from users, it is responsible for rendering a response
+///with the aid of the ItemSpecsView and ItemSpecsController.
+public class ItemSpecsController {
+	/// Constructor
+	public ItemSpecsController() {
+
+	}
+}
