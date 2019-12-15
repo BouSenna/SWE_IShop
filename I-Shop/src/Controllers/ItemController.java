90014026ac16d02a7709619308972dbe886beaf7
BouSenna <sara_usa_1999@hotmail.com>
2019-12-15 02:38:31 +0200

    added addItem method.

--- I-Shop/src/Controllers/ItemController.java
+++ I-Shop/src/Controllers/ItemController.java
@@ -1,7 +1,13 @@
 package Controllers;
 
+import Models.ItemModel;
+
 ///This class handles requests from users, it is responsible for rendering a response
 ///with the aid of the ItemView and ItemController.
 public class ItemController {
 	public ItemController() {}
+	
+	public void addItem(int minPrice, int maxPrice) {
+		new ItemModel().addItem(minPrice, maxPrice);
+	}
 }
