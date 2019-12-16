6c9de1bb04708a9ae82bf6c40c000466e036835e
BouSenna <sara_usa_1999@hotmail.com>
2019-12-17 00:45:54 +0200

    Added class ProductController.

---
+++ I-Shop/src/Controllers/ProductController.java
@@ -0,0 +1,10 @@
+package Controllers;
+
+//This class handles requests from users, it is responsible for rendering a response
+///with the aid of the ProductView and ProductController.
+public class ProductController {
+	public ProductController() {
+
+	}
+
+}
