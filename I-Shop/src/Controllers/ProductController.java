101495592ee3ea0eb4414b3be1f622c81f2794cc
BouSenna <sara_usa_1999@hotmail.com>
2019-12-17 00:50:07 +0200

    Added addProduct methods.

--- I-Shop/src/Controllers/ProductController.java
+++ I-Shop/src/Controllers/ProductController.java
@@ -1,5 +1,10 @@
 package Controllers;
 
+import Forms.DisplayMessages;
+import Models.BrandModel;
+import Models.ItemSpecsModel;
+import Models.ProductModel;
+
 //This class handles requests from users, it is responsible for rendering a response
 ///with the aid of the ProductView and ProductController.
 public class ProductController {
@@ -7,4 +12,22 @@ public class ProductController {
 
 	}
 
+	/// Adding new product to the database given the brand and item name.
+	public void addProduct(String name, int price, int store, String brandName, String itemName, int quantity) {
+		/// Getting the ID if both the brand and the item.
+		int BrandID = new BrandModel().getBrandID(brandName);
+		int ItemID = new ItemSpecsModel().getItemID(itemName);
+
+		/// Adding the new product to the DB.
+		addProduct(name, price, store, BrandID, ItemID, quantity);
+	}
+
+	/// Adding new product to the database
+	public void addProduct(String name, int price, int store, int brand, int item, int quantity) {
+		/// Adding the new product to the DB.
+		new ProductModel().addNewProduct(brand, item, store, price, name, quantity, quantity);
+
+		/// Sending verification message.
+		new DisplayMessages().displayMessage("Product added successfully to your store.");
+	}
 }
