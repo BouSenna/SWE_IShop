8f5ca795d8b4b4889ebb3f9d6a14090bed20f1e6
BouSenna <sara_usa_1999@hotmail.com>
2019-12-16 22:46:28 +0200

    Added class AddProduct that implements IStoreOwnerCommand.

---
+++ I-Shop/src/StoreOwnerCommands/AddProductCommand.java
@@ -0,0 +1,23 @@
+package StoreOwnerCommands;
+
+public class AddProductCommand implements IStoreOwnerCommand {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void undo() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
