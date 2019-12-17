81e19553220c1f9b12e2c2fa1bfc59903e32057f
BouSenna <sara_usa_1999@hotmail.com>
2019-12-17 02:12:53 +0200

    Added button to open new form for store owner privileges.

--- I-Shop/src/Forms/StoreOwnerHomepageForm.java
+++ I-Shop/src/Forms/StoreOwnerHomepageForm.java
@@ -7,7 +7,11 @@ import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.ActionEvent;
 
 public class StoreOwnerHomepageForm {
@@ -30,12 +34,12 @@ public class StoreOwnerHomepageForm {
 		frame.setBounds(100, 100, 880, 505);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("I-Shop Store-Owner Home");
 		lblNewLabel.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 35));
 		lblNewLabel.setBounds(185, 16, 858, 39);
 		frame.getContentPane().add(lblNewLabel);
-		
+
 		JButton addStore_button = new JButton("Add new store");
 		addStore_button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -48,7 +52,7 @@ public class StoreOwnerHomepageForm {
 		addStore_button.setBackground(Color.LIGHT_GRAY);
 		addStore_button.setBounds(313, 206, 237, 39);
 		frame.getContentPane().add(addStore_button);
-		
+
 		JButton addProduct_button = new JButton("Add new product");
 		addProduct_button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -61,7 +65,7 @@ public class StoreOwnerHomepageForm {
 		addProduct_button.setBackground(Color.LIGHT_GRAY);
 		addProduct_button.setBounds(313, 151, 237, 39);
 		frame.getContentPane().add(addProduct_button);
-		
+
 		JButton viewStat_button = new JButton("View Statistics");
 		viewStat_button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -74,20 +78,36 @@ public class StoreOwnerHomepageForm {
 		viewStat_button.setBackground(Color.LIGHT_GRAY);
 		viewStat_button.setBounds(313, 261, 237, 39);
 		frame.getContentPane().add(viewStat_button);
-		
+
 		JButton btnAddCollaborator = new JButton("Add collaborator");
 		btnAddCollaborator.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				frame.setVisible(false);
 				AddCollaborator newCollab = new AddCollaborator();
-				
+
 			}
-			
+
 		});
 		btnAddCollaborator.setForeground(Color.BLACK);
 		btnAddCollaborator.setFont(new Font("Corbel", Font.PLAIN, 23));
 		btnAddCollaborator.setBackground(Color.LIGHT_GRAY);
 		btnAddCollaborator.setBounds(313, 315, 237, 39);
 		frame.getContentPane().add(btnAddCollaborator);
+
+		JLabel lblNewLabel_1 = new JLabel("Store owner privileges");
+		lblNewLabel_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				String input = JOptionPane.showInputDialog("Enter the store password please");
+				if (input.equals("owner123")) {
+					frame.setVisible(false);
+					CollabActionsTracker newForm = new CollabActionsTracker();
+				}
+				else new DisplayMessages().displayMessage("Not supported for this account.");
+			}
+		});
+		lblNewLabel_1.setForeground(Color.RED);
+		lblNewLabel_1.setBounds(15, 413, 193, 20);
+		frame.getContentPane().add(lblNewLabel_1);
 	}
 }
