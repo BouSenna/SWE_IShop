64f6d29423609e6e04aa8a54b783be5675cd29fe
BouSenna <sara_usa_1999@hotmail.com>
2019-12-17 16:40:44 +0200

    Added object from CollabActionsTracker to the undoHandler.

--- I-Shop/src/StoreOwnerCommands/UndoHandler.java
+++ I-Shop/src/StoreOwnerCommands/UndoHandler.java
@@ -2,13 +2,17 @@ package StoreOwnerCommands;
 
 import java.util.Stack;
 
+import Forms.CollabActionsTracker;
+
 public class UndoHandler {
 	/// Stack to store the history in it so that you can undo changes.
-	private Stack<IStoreOwnerCommand> undoStack;
+	public Stack<IStoreOwnerCommand> undoStack;
+	public CollabActionsTracker tracker;
 
 	/// Constructor
 	public UndoHandler() {
 		this.undoStack = new Stack<IStoreOwnerCommand>();
+		tracker = new CollabActionsTracker(this);
 	}
 
 	/// Method to execute and store the command in the stack.
@@ -18,11 +22,12 @@ public class UndoHandler {
 			command.execute();
 			/// pushing the command in the stack in case we needed later [undo]
 			undoStack.push(command);
+			tracker.addUpdates(command.getName());
 		} catch (IllegalStateException e) {
 		}
 	}
 
-	/// Method to un-execute the last command and poping it from the stack.
+	/// Method to un-execute the last command and popping it from the stack.
 	public void undo() {
 		/// Checking if there isn't any command executed yet.
 		if (!undoStack.isEmpty()) {
